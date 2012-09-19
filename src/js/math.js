dd6e9ffdf77b0e038671429411e71afdcf708d4b
Rob Friesel <robf@dealer.com>
2012-09-18 21:57:06 -0400

    better comments around our JS

--- src/js/math.js
+++ src/js/math.js
@@ -2,6 +2,14 @@
 
 vtcc.math = (function(){
 
+  /**
+   * Takes any number of arguments (of any type) and prepares them for
+   * consumption by other methods in this suite.
+   *
+   * @param {Mixed/Variable}
+   * @return {Array} sanitized arguments
+   * @private
+   */
   function _prepMathArgs(){
     var args = vtcc.util.flatten.apply(this, arguments),
         sanitizedArgs = [];
@@ -15,10 +23,24 @@ vtcc.math = (function(){
     return sanitizedArgs;
   }
 
+  /**
+   * Takes any number of arguments (of any type), sanitizes them to just numbers,
+   * and returns the largest value.
+   *
+   * @param {Mixed/Variable}
+   * @return {Number}
+   */
   function max(){
     return Math.max.apply(Math, _prepMathArgs.apply(this, arguments));
   }
 
+  /**
+   * Takes any number of arguments (of any type), sanitizes them to just numbers,
+   * and returns the smallest value.
+   *
+   * @param {Mixed/Variable}
+   * @return {Number}
+   */
   function min(){
     return Math.min.apply(Math, _prepMathArgs.apply(this, arguments));
   }
--- src/js/util.js
+++ src/js/util.js
@@ -5,6 +5,10 @@ vtcc.util = (function(){
 
   /**
    * Naively and optimistcally flattens an Array -- recursively!
+   *
+   * @param {Mixed/Variable} Takes any number of arguments (of any type) and
+   * attempts to merge them into a flattened/1D Array.
+   * @return {Array}
    */
   function flatten(){
     var args = Array.prototype.slice.call(arguments, 0),
@@ -22,6 +26,12 @@ vtcc.util = (function(){
     return flattenedArgs;
   }
 
+  /**
+   * Tests an arbitrary argument for Array-ness.
+   *
+   * @param {Mixed}
+   * @return {Boolean}
+   */
   function isArray(a){
     return Object.prototype.toString.call(a) === '[object Array]';
   }
